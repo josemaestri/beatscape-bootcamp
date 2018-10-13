03a32d032faf8b9210e0a3311128301c3d32fd72
josemaestri <maestri.jose@gmail.com>
2018-10-12 21:11:31 -0400

    dev folder and users dev files

---
+++ dev/users.js
@@ -0,0 +1,29 @@
+// FIREBASE
+// ==========================
+var config = {
+  apiKey: "AIzaSyDN33XuMZh2HgwOFk7_-tagJ0-ggUPD9Zw",
+  authDomain: "beatscape-48240.firebaseapp.com",
+  databaseURL: "https://beatscape-48240.firebaseio.com",
+  projectId: "beatscape-48240",
+  storageBucket: "",
+  messagingSenderId: "912101144942"
+};
+firebase.initializeApp(config);
+
+// VARS
+// ==========================
+
+
+// HTML VARS
+// ==========================
+
+
+// FUNCTIONS
+// ==========================
+
+
+
+
+
+// RUN THE TRAPPPPP
+// ==========================
