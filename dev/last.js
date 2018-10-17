baf694bd4c6bf5890eef07e8dd0670a7107a91ed
josemaestri <maestri.jose@gmail.com>
2018-10-16 23:32:38 -0400

    testing last.fm api

--- dev/last.js
+++ dev/last.js
@@ -2,7 +2,7 @@
 
 $("#add-artist").on("click", function(event){
    event.preventDefault();
-})
+});
 
 // Click Submit-Button //
 
@@ -22,7 +22,27 @@ function loadDoc(artistName) {
 
    $.ajax({url:queryURL,method:'GET'})
    .done(function(response){
-       console.table(response);
-       // $('#artist').prepend("<p>Artist: "+response.data[i].artist+"</p>");
+       if(response.page.totalPages<1){
+           console.log("Artist not Available");
+       }
+       else{
+           console.table(response._embedded.attractions[0]);
+           if(response._embedded.attractions[0].upcomingEvents._total>0){
+               getEvents(response._embedded.attractions[0].id);
+           }
+           else{console.log("Artist Has No Upcoming Shows")};
+       }
    });
-};
+}
+
+function getEvents(artistId){
+   var queryURL="https://app.ticketmaster.com/discovery/v2/events.json?apikey=EmeiA0oiWGnbz7iXCD6XGf5YLb9StWec&attractionId="+artistId;
+
+   $.ajax({
+    url:queryURL,
+    method:'GET'
+  })
+  .done(function(response){
+     console.table(response._embedded.events);
+  });
+}
